5d6285b767dd1aac3a281dfc0bbde729ca4ebe61
rtlearn <76143377+Anshul66@users.noreply.github.com>
2021-07-17 02:20:17 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'internshala_cwh');
+define('DB_USERNAME', 'thegreen_anshul');
+define('DB_PASSWORD', 'Anshul@123');
+define('DB_NAME', 'thegreen_internshala_cwh');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
